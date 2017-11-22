c13e7910420e22de2ebb6df7e235e58e2f4b75fc
cabbitest <evacaribbeansea@gmail.com>
2017-11-22 14:58:58 +0100

    update

--- b02WazzupCls.java
+++ b02WazzupCls.java
@@ -35,11 +35,11 @@ class b02WazzupCls	 {
 	Java
 	Here, the WazzupCls.java emulates the behaviour of a cls. 
 
-    i=0     means that "i" starts at 0
-    i<60    ... and loops 60 times (less than 60, starts at 0)
-    i++     ... every time increasing with 1. 
+	i=0     means that "i" starts at 0
+	i<60    ... and loops 60 times (less than 60, starts at 0)
+	i++     ... every time increasing with 1. 
 
-    println();	
+	println();	
             means to print something on an own line (new line). And as this line is empty 
             there will be 60 empty lines before the prompt line shows up again.
 
