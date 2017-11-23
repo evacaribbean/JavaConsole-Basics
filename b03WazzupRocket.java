7ba7b8884b5dc0fe45f0caa28ee94cca4d1f8e81
cabbitest <evacaribbeansea@gmail.com>
2017-11-23 17:34:34 +0100

    update

--- b03WazzupRocket.java
+++ b03WazzupRocket.java
@@ -29,12 +29,12 @@ class b03WazzupRocket {
  		System.out.println("                       0                                          \\/ ' * '| |");
  		System.out.println("                  q         0  * l                                   | * || |");
  		System.out.println("                                o                                    '___' #");
- 		System.out.println("                  * p  *                                             (   )          ...yhaaow, Long time no 0!");
+ 		System.out.println("                  * p  *  a                                          (   )          ...yhaaow, Long time no 0!");
  		System.out.println("                                                                     | | |");
  		System.out.println("                    ~      H O                                       | | |");  
- 		System.out.println("                            2                                       [ ][ ]");
- 		System.out.println("                        '                                            | | |");
- 		System.out.println("                                                                    /  ][ \\");
- 		System.out.println("''''''''''''''''''''''' ''''' '''''' ''''''''''''  '''' ''''' ''  '''''''' ''''''''''''''' ''''  ' '''''' ''' '");  	 
+ 		System.out.println("           .                2                                       [ ][ ]");
+ 		System.out.println("       .             b  '                                            | | |");
+ 		System.out.println("                         .      ~                                   /  ][ \\");
+ 		System.out.println("''''d'.'''''''''p'' '*''o' ''a'' '.''~'''''  '''' ''''' ''  '''''''' ''''''''''''''' ''''  ' '''''' ''' '");  	 
 	}
-} 
+} 	
