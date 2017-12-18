b9a2c1d88693612c3a40a12af6f47a962bef2019
cabbitest <evacaribbeansea@gmail.com>
2017-12-18 21:08:33 +0100

    first commit

---
+++ b04FortuneNumTelling.java
@@ -0,0 +1,174 @@
+import java.util.Scanner;
+
+class Po {
+
+	public static void main(String[] args) {
+
+		Scanner userMessage = new Scanner(System.in);
+	 	int n1; 
+	 	String s1 = "* A thrilling time is in your immediate future *";
+	 	String s2 = "* Your fondest dream will come true within this year *";
+	 	String s3 = "* You have a deep interest in all that is artistic *";
+	 	String s4 = "* Good health will be yours for a long time *";
+	 	String s5 = "* Failure is only the opportunity to begin again more intelligently *";
+	 	String s6 = "* Never upset the driver of the car you're in; they're the master of your destiny until you get home *";
+	 	String s7 = "* You only need look to your own reflection for inspiration. Because you are Beautiful! *";
+	 	String s8 = "* Rivers need springs *";
+	 	String s9 = "* Good news from afar may bring you a welcome visitor *";
+	 	String s10 = "* You already know the answer to the questions lingering inside your head *";
+	 	String s11 = "* A single conversation with a wise man is better than ten years of study *";
+	 	String s12 = "* Integrity is doing the right thing, even if nobody is watching *";
+	 	String s13 = "* Your life will be filled with magical moments *";
+ 	 	String s14 = "* You will have a pleasant surprise *";
+	 	String s15 = "* You will have unexpected great good luck *";
+	 	String s16 = "* You are extremely loved. Don't worry :) *";
+	 	String s17 = "* The world is always ready to receive talent with open arms *";
+	 	String s18 = "* Intelligence is the door to freedom and alert attention is the mother of intelligence *";
+	 	String s19 = "* Express yourself: Don't hold back! *";
+	 	String s20 = "* It is not necessary to show others you have change; the change will be obvious *";
+	 	String s21 = "* The time is alway right to do what is right *";
+	 	String s22 = "* Not all closed eye is sleeping nor open eye is seeing *";
+	 	String s23 = "* Whenever possible, keep it simple *";
+	 	String s24 = "* Your dearest wish will come true *";
+	 	String s25 = "* Your smile brings happiness to everyone you meet *";
+	 	String s26 = "* It's amazing how much good you can do if you don't care who gets the credit *";
+	 	String s27 = "* A very attractive person has a message for you *";
+	 	String s28 = "* Do not fear what you don't know *";
+	 	String s29 = "* Your high-minded principles spell success *";
+	 	String s30 = "* Change can hurt, but it leads a path to something better *";
+	 	String s31 = "* Enjoy the good luck a companion brings you *";
+	 	String s32 = "* People are naturally attracted to you *";
+	 	String s33 = "* Hidden in a valley beside an open stream- This will be the type of place where you will find your dream *";
+	 	String s34 = "* What ever you're goal is in life, embrace it visualize it, and for it will be yours *";
+	 	String s35 = "* Your shoes will make you happy today *";
+	 	String s36 = "* Be on the lookout for coming events; They cast their shadows beforehand *";
+	 	String s37 = "* Land is always on the mind of a flying bird *";
+	 	String s38 = "* Meeting adversity well is the source of your strength *";
+	 	String s39 = "* A dream you have will come true *";
+	 	String s40 = "* There is no greater pleasure than seeing your loved ones prosper *";
+	 	String s41 = "* It is now, and in this world, that we must live *";
+	 	String s42 = "* You can make your own happiness *";
+	 	String s43 = "* The greatest risk is not taking one *";
+	 	String s44 = "* Love can last a lifetime, if you want it to *";
+	 	String s45 = "* Wealth awaits you very soon *";
+	 	String s46 = "* A stranger, is a friend you have not spoken to yet *";
+	 	String s47 = "* You are very talented in many ways *";
+	 	String s48 = "* A new voyage will fill your life with untold memories *";
+	 	String s49 = "* You will travel to many exotic places in your lifetime *";
+	 	String s50 = "* Let your acts speak *"; 
+
+	 	String t = "The Fortune Teller";
+	 	String c = "hoose a number between 1-50  ";
+	 	String u = "Your number ";
+	 	String s = " says  ";
+	 	String e = "\n\t Try again, c" + c;	 	
+
+		System.out.println("\n\n\t" + t + "\n"); 
+		System.out.print("\tC" + c);
+		 
+		n1 = userMessage.nextInt();
+
+		if (n1 == 1)
+			System.out.println("\n\t" + u + n1 + s + s1 + "\n\n\n");
+		else if (n1 == 2)
+			System.out.println("\n\t" + u + n1 + s + s2 + "\n\n\n"); 
+		else if (n1 == 3)
+			System.out.println("\n\t" + u + n1 + s + s3 + "\n\n\n");  
+		else if (n1 == 4)
+			System.out.println("\n\t" + u + n1 + s + s4 + "\n\n\n");   
+		else if (n1 == 5)
+			System.out.println("\n\t" + u + n1 + s + s5 + "\n\n\n"); 
+		else if (n1 == 6)
+			System.out.println("\n\t" + u + n1 + s + s6 + "\n\n\n"); 
+		else if (n1 == 7)
+			System.out.println("\n\t" + u + n1 + s + s7 + "\n\n\n"); 
+		else if (n1 == 8)
+			System.out.println("\n\t" + u + n1 + s + s8 + "\n\n\n");  
+		else if (n1 == 9)
+			System.out.println("\n\t" + u + n1 + s + s9 + "\n\n\n");  
+		else if (n1 == 10)
+			System.out.println("\n\t" + u + n1 + s + s10 + "\n\n\n");  
+		else if (n1 == 11)
+			System.out.println("\n\t" + u + n1 + s + s11 + "\n\n\n"); 
+		else if (n1 == 12)
+			System.out.println("\n\t" + u + n1 + s + s12 + "\n\n\n");  
+		else if (n1 == 13)
+			System.out.println("\n\t" + u + n1 + s + s13 + "\n\n\n"); 
+		else if (n1 == 14)
+			System.out.println("\n\t" + u + n1 + s + s14 + "\n\n\n");  
+		else if (n1 == 15)
+			System.out.println("\n\t" + u + n1 + s + s15 + "\n\n\n"); 
+		else if (n1 == 16)
+			System.out.println("\n\t" + u + n1 + s + s16 + "\n\n\n");  
+		else if (n1 == 17)
+			System.out.println("\n\t" + u + n1 + s + s17 + "\n\n\n"); 
+		else if (n1 == 18)
+			System.out.println("\n\t" + u + n1 + s + s18 + "\n\n\n");  
+		else if (n1 == 19)
+			System.out.println("\n\t" + u + n1 + s + s19 + "\n\n\n"); 		
+		else if (n1 == 20)
+			System.out.println("\n\t" + u + n1 + s + s20 + "\n\n\n");  
+		else if (n1 == 21)
+			System.out.println("\n\t" + u + n1 + s + s21 + "\n\n\n"); 
+		else if (n1 == 22)
+			System.out.println("\n\t" + u + n1 + s + s22 + "\n\n\n");  
+		else if (n1 == 23)
+			System.out.println("\n\t" + u + n1 + s + s23 + "\n\n\n"); 
+		else if (n1 == 24)
+			System.out.println("\n\t" + u + n1 + s + s24 + "\n\n\n");  
+		else if (n1 == 25)
+			System.out.println("\n\t" + u + n1 + s + s25 + "\n\n\n"); 
+		else if (n1 == 26)
+			System.out.println("\n\t" + u + n1 + s + s26 + "\n\n\n");  
+		else if (n1 == 27)
+			System.out.println("\n\t" + u + n1 + s + s27 + "\n\n\n"); 
+		else if (n1 == 28)
+			System.out.println("\n\t" + u + n1 + s + s28 + "\n\n\n");  
+		else if (n1 == 29)
+			System.out.println("\n\t" + u + n1 + s + s29 + "\n\n\n"); 
+		else if (n1 == 30)
+			System.out.println("\n\t" + u + n1 + s + s30 + "\n\n\n");  
+		else if (n1 == 31)
+			System.out.println("\n\t" + u + n1 + s + s31 + "\n\n\n"); 
+		else if (n1 == 32)
+			System.out.println("\n\t" + u + n1 + s + s32 + "\n\n\n");  
+		else if (n1 == 33)
+			System.out.println("\n\t" + u + n1 + s + s33 + "\n\n\n"); 
+		else if (n1 == 34)
+			System.out.println("\n\t" + u + n1 + s + s34 + "\n\n\n");  
+		else if (n1 == 35)
+			System.out.println("\n\t" + u + n1 + s + s35 + "\n\n\n"); 
+		else if (n1 == 36)
+			System.out.println("\n\t" + u + n1 + s + s36 + "\n\n\n");  
+		else if (n1 == 37)
+			System.out.println("\n\t" + u + n1 + s + s37 + "\n\n\n"); 
+		else if (n1 == 38)
+			System.out.println("\n\t" + u + n1 + s + s38 + "\n\n\n");  
+		else if (n1 == 39)
+			System.out.println("\n\t" + u + n1 + s + s39 + "\n\n\n"); 
+		else if (n1 == 40)
+			System.out.println("\n\t" + u + n1 + s + s40 + "\n\n\n");  
+		else if (n1 == 41)
+			System.out.println("\n\t" + u + n1 + s + s41 + "\n\n\n"); 
+		else if (n1 == 42)
+			System.out.println("\n\t" + u + n1 + s + s42 + "\n\n\n");  
+		else if (n1 == 43)
+			System.out.println("\n\t" + u + n1 + s + s43 + "\n\n\n"); 
+		else if (n1 == 44)
+			System.out.println("\n\t" + u + n1 + s + s44 + "\n\n\n");  
+		else if (n1 == 45)
+			System.out.println("\n\t" + u + n1 + s + s45 + "\n\n\n"); 
+		else if (n1 == 46)
+			System.out.println("\n\t" + u + n1 + s + s46 + "\n\n\n");  
+		else if (n1 == 47)
+			System.out.println("\n\t" + u + n1 + s + s47 + "\n\n\n"); 
+		else if (n1 == 48)
+			System.out.println("\n\t" + u + n1 + s + s48 + "\n\n\n");  
+		else if (n1 == 49)
+			System.out.println("\n\t" + u + n1 + s + s49 + "\n\n\n"); 
+		else if (n1 == 50)
+			System.out.println("\n\t" + u + n1 + s + s50 + "\n\n\n");  
+		else  
+			System.out.println(e);
+	}
+} 
